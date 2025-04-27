32e29bd0bc5b53ef58aa1a91e68f226c5506e3cd
DelusionCB <59691622+DelusionCB@users.noreply.github.com>
2025-04-27 15:22:58 +0300

    Fixed layouts
    Fixed layouts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import PHProvider from "./providers";
+// import PHProvider from "./providers";
 import { AuthProvider } from "@/components/AuthProvider";
 import { Toaster } from "@/components/ui/toaster";
 import CookieBanner from "@/components/CookieBanner";
-import { AvailableLocale } from "@/lib/dictionaries";
+import {AvailableLocale, getDictionary} from "@/lib/dictionaries";
 import { Inter } from "next/font/google";
+import {Footer, Navigation, PromotionHeader} from "@/components/lander";
+// import PostHogPageView from "@/app/ClientOnlyPHProvider";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -18,28 +20,36 @@ export const metadata: Metadata = {
 };
 
 export default async function RootLayout(
-  props: Readonly<{
-    children: React.ReactNode;
-    params: Promise<{ lang: AvailableLocale }>;
-  }>
+    props: Readonly<{
+      children: React.ReactNode;
+      params: { lang: AvailableLocale };
+    }>
 ) {
-  const params = await props.params;
-
-  const { lang } = params;
+  const { lang } = await props.params;
 
   const { children } = props;
 
+  const dict = await getDictionary(lang);
   return (
     <html lang={lang || "fi"} className={inter.className}>
-      <PHProvider>
+    <AuthProvider>
+      {/*<PHProvider>*/}
         <body className={`bg-white text-black ${inter.className} antialiased`}>
-          <CookieBanner />
-          <AuthProvider>
-            <Toaster />
-            {children}
-          </AuthProvider>
+        <div className="flex flex-col min-h-screen">
+          <header className="bg-white shadow-sm">
+            <PromotionHeader dict={dict}/>
+            <Navigation dict={dict}/>
+          </header>
+          <CookieBanner/>
+            <main className="flex-grow xl:px-24 lg:px-0 md:px-12 bg-white">
+              <Toaster/>
+              {children}
+            </main>
+          <Footer dict={dict}/>
+        </div>
         </body>
-      </PHProvider>
+      {/*</PHProvider> */}
+    </AuthProvider>
     </html>
-  );
+);
 }
